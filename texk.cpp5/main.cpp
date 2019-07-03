20ea3b3500f040190e5b9e90dcba9e76e6338c26
zengkaiwei2018 <1912207369@qq,com>
2019-07-03 18:39:40 +0800

    Initial Commit

---
+++ texk.cpp5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  texk.cpp5
+//
+//  Created by s20181105306 on 2019/7/3.
+//  Copyright © 2019 s20181105306. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
